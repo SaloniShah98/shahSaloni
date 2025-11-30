63ca9370e40d19845037300d658fa69a6fb04e22
SaloniShah98 <shahsallu98@gmail.com>
2025-11-30 22:11:25 +0530

    updated navbar, font-family and routes

--- my-portfolio/src/app/app.routes.ts
+++ my-portfolio/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { About } from './about/about';
 import { Projects } from './projects/projects';
+import { Experience } from './experience/experience';
+import { Contact } from './contact/contact';
 
 export const routes: Routes = [
     { path: 'about', component: About },
     { path: 'projects', component: Projects },
+    { path: 'experience', component: Experience },
+    { path: 'contact', component: Contact },
     { path: '', redirectTo: '/about', pathMatch: 'full' }
 ];
